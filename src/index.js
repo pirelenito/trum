a6aec4545a8720295f2bfdab9bc66963fa577fdc
Paulo Ragonha <paulo@ragonha.me>
2017-06-25 15:40:31 +0200

    React hello world 🌏

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-import style from './index.css'
+import React from 'react'
+import { render } from 'react-dom'
 
-const div = document.createElement('div')
-div.innerHTML = '<h1>Hello World</h1>'
-div.className = style.component
-
-document.getElementById('root').appendChild(div)
+render(<h1>Hello World</h1>, document.getElementById('root'))
