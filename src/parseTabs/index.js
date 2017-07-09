5ccdf2d44504f89a78b9e8e494a1dca3c737fed9
Paulo Ragonha <paulo@ragonha.me>
2017-07-09 19:10:28 +0200

    Allow bigger label spacing on tabs 🙌

--- src/parseTabs/index.js
+++ src/parseTabs/index.js
@@ -1,6 +1,6 @@
 export default tabs => {
   return tabs.split('\n').filter(line => line.trim().length > 0).map(line => {
-    const match = line.match(/^(.{1,2})\|(.+)/)
+    const match = line.match(/^(.{1,3})\|(.+)/)
 
     return !match
       ? { symbol: 'invalid', notes: [] }
--- src/parseTabs/index.spec.js
+++ src/parseTabs/index.spec.js
@@ -5,6 +5,7 @@ const fixture = [
   'Rd|c-c-----|--------|   ',
   'S |----o---|----o---|',
   'F|--------|----o-oo|',
+  'Y  |----o---|----o---|',
 ].join('\n')
 
 describe('tabs', () => {
@@ -22,10 +23,15 @@ describe('tabs', () => {
         symbol: 'f',
         notes: ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', 'o', '-', 'o', 'o'],
       },
+      {
+        symbol: 'y',
+        notes: ['-', '-', '-', '-', 'o', '-', '-', '-', '-', '-', '-', '-', 'o', '-', '-', '-'],
+      },
     ]
 
     expect(parseTabs(fixture)[0]).toEqual(expected[0])
     expect(parseTabs(fixture)[1]).toEqual(expected[1])
     expect(parseTabs(fixture)[2]).toEqual(expected[2])
+    expect(parseTabs(fixture)[3]).toEqual(expected[3])
   })
 })
