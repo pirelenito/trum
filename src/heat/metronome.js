1f0a923f705f8400e58c17c7e9baf02eec7fcdf5
Xavier Via <fernando.via@gmail.com>
2017-08-01 21:09:09 +0200

    Metro Metro Metronome

--- src/heat/metronome.js
+++ src/heat/metronome.js
@@ -1,19 +1,36 @@
-import { getBpm } from '../store'
-import * as metronome from '../metronome'
+import '../store'
+
+const { floor } = Math
+
+let previousTime = 0
+
+function playTone(audioContext, pitch = 440) {
+  var osc = audioContext.createOscillator()
+  osc.connect(audioContext.destination)
+
+  const scheduleTime = findProperTime(0.5, audioContext.currentTime)
+  console.log(audioContext.currentTime, scheduleTime)
+  osc.frequency.value = pitch
+  osc.start(scheduleTime)
+  osc.stop(scheduleTime + 0.1)
+  previousTime = scheduleTime
+}
 
 export default state$ => {
-  metronome.init()
-  metronome.play()
+  const audioContext = new AudioContext()
 
   return (
     state$
-      .map(getBpm)
+      .map(({ now }) => now)
+      .map(time => floor(time / 500))
       .skipRepeats()
-      .tap(bpm => {
-        metronome.setTempo(bpm)
-        // where the magic happens
+      .tap(time => {
+        console.log(time)
+        playTone(audioContext)
       })
       // we don't want to do anything
       .filter(() => false)
   )
 }
+
+const findProperTime = (step, time) => (floor(time / step) + 1) * step
