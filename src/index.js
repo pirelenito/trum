e0a6c002740c979b676dea852bf8277a19420b19
Paulo Ragonha <paulo@ragonha.me>
2018-10-21 19:42:58 +0200

    Make everything look prettier
    Based on https://tympanus.net/codrops/2016/04/26/the-aviator-animating-basic-3d-scene-threejs/

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import * as THREE from 'three'
 import './index.css'
 import createTrack from './renderer/createTrack'
+import createLights from './renderer/createLights'
 
 const scene = new THREE.Scene()
+scene.fog = new THREE.Fog(0xf7d9aa, 100, 950)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+
+const lights = createLights()
+lights.forEach(light => scene.add(light))
 
 const track = createTrack(['c', '-', 'c', '-', 'c', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'])
 scene.add(track)
@@ -19,7 +24,8 @@ camera.rotation.x = 1.5
 console.log(scene, camera)
 
 function animate() {
-  // track.position.y -= 0.1
+  track.position.y -= 0.1
+  // track.rotation.x -= 0.01
 
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
--- src/renderer/createTrack.js
+++ src/renderer/createTrack.js
@@ -1,8 +1,12 @@
 import * as THREE from 'three'
 
 function createNote(type, index) {
-  const geometry = new THREE.BoxGeometry(1.5, 0.7, 0.5)
-  const material = new THREE.MeshBasicMaterial({ color: type === '-' ? 0xffffff : 0xf7a59c })
+  const geometry = new THREE.BoxGeometry(1.5, 0.7, 0.2, 1, 1, 1)
+  const material = new THREE.MeshPhongMaterial({
+    color: type === '-' ? 0xffffff : 0xf7a59c,
+    shading: THREE.FlatShading,
+  })
+
   const note = new THREE.Mesh(geometry, material)
 
   note.position.y = index * 10
