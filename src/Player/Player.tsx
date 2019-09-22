b89cf947be89eeaf62ff639dedee2e2770c4713f
Paulo Ragonha <paulo@ragonha.me>
2019-09-22 20:51:56 +0200

    Simple mapping of the “bass drum” as a line 👨‍🎨

--- src/Player/Player.tsx
+++ src/Player/Player.tsx
@@ -31,7 +31,9 @@ function InnerPlayer({ parsedTabs }: PlayerProps) {
   const groupRef = useRef<THREE.Group>(null)
   const startRef = useRef(0)
   const playStepRef = useRef<PlayFunction | null>(null)
-  const speed = 100
+  const noteBoxMap = [1, 2, 3, 4, 5, 6, 7]
+  const noteLineIndex = 0
+  const speed = 300
 
   useEffect(() => {
     if (!parsedTabs) return
@@ -46,8 +48,6 @@ function InnerPlayer({ parsedTabs }: PlayerProps) {
     const group = groupRef.current
     if (!group || !startRef.current || !playStepRef.current) return
 
-    // console.log('timestamp', timestamp)
-
     const songPosition = (Date.now() - startRef.current) / speed
     const position = Math.floor(songPosition)
     playStepRef.current(position)
@@ -56,30 +56,40 @@ function InnerPlayer({ parsedTabs }: PlayerProps) {
   })
 
   if (!parsedTabs) return null
+
   return (
     <>
       <hemisphereLight args={[0xaaaaaa, 0x000000, 0.9]} />
       <ambientLight intensity={0.5} />
-      <group rotation={[-0.9, 0, 0]} position={[(-parsedTabs.instruments.length * 0.8) / 2, -1.5, 1.5]}>
+      <group rotation={[-0.9, 0, 0]} position={[(-noteBoxMap.length * 0.8) / 2, -1.5, 1.5]}>
         <group>
-          {parsedTabs.instruments.map((instrument, noteIndex) => (
-            <mesh position={[noteIndex * 1, 0, -0.09]} key={noteIndex}>
+          {noteBoxMap.map((instrumentIndex, orderIndex) => (
+            <mesh position={[orderIndex * 1, 0, -0.09]} key={instrumentIndex}>
               <boxGeometry attach="geometry" args={[0.8, 0.3, 0.01, 1, 1, 1]} />
-              <meshPhongMaterial attach="material" color={COLORS[noteIndex % COLORS.length]} />
+              <meshPhongMaterial attach="material" color={COLORS[instrumentIndex % COLORS.length]} />
             </mesh>
           ))}
         </group>
         <group ref={groupRef}>
           {parsedTabs.notes.map((notes, index) => {
+            const noteLine = notes[noteLineIndex]
+
             return (
               <group key={index} position={[0, index, 0]}>
-                {notes.map((note, noteIndex) =>
-                  !note ? null : (
-                    <mesh position={[noteIndex * 1, 0, 0]} key={noteIndex}>
+                {noteBoxMap.map((instrumentIndex, orderIndex) => {
+                  const note = notes[instrumentIndex]
+                  return !note ? null : (
+                    <mesh position={[orderIndex * 1, 0, 0]} key={instrumentIndex}>
                       <boxGeometry attach="geometry" args={[0.8, 0.3, 0.2, 1, 1, 1]} />
-                      <meshPhongMaterial attach="material" color={COLORS[noteIndex % COLORS.length]} />
+                      <meshPhongMaterial attach="material" color={COLORS[instrumentIndex % COLORS.length]} />
                     </mesh>
-                  ),
+                  )
+                })}
+                {!noteLine ? null : (
+                  <mesh position={[noteBoxMap.length / 2 - 0.5, 0, -0.05]} key={noteLineIndex}>
+                    <boxGeometry attach="geometry" args={[noteBoxMap.length - 0.4, 0.1, 0.05, 1, 1, 1]} />
+                    <meshPhongMaterial attach="material" color={COLORS[noteLineIndex % COLORS.length]} />
+                  </mesh>
                 )}
               </group>
             )
